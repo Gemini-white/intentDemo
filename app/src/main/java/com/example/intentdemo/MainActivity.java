d4df376d682f3e42c4f544768e4a537450f82112
cheng <2493793849@qq.com>
2021-05-31 14:30:22 +0800

    创建了HelloActivity，用于显示跳转演示

--- app/src/main/java/com/example/intentdemo/MainActivity.java
+++ app/src/main/java/com/example/intentdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.intentdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.intentdemo.databinding.ActivityMainBinding;
