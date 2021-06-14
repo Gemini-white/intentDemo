e2e0eccc9528881897fe376ff794a693612982e7
cheng <2493793849@qq.com>
2021-06-14 19:41:13 +0800

    实现了登录(返回值)功能 尚未完善，有缺陷

--- app/src/main/java/com/example/intentdemo/LoginActivity.java
+++ app/src/main/java/com/example/intentdemo/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.intentdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -14,6 +15,8 @@ import com.example.intentdemo.databinding.ActivityLoginBinding;
 public class LoginActivity extends AppCompatActivity {
 
     private ActivityLoginBinding mBinding;
+    private static final String INTENT_TEXT1="intent_text1";
+    private String text1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,5 +52,36 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        mBinding.ButtonLogin3.setOnClickListener(v -> {
+            String user = mBinding.user.getText().toString();
+            String pwd = mBinding.pwd.getText().toString();
+            String isAdmin = mBinding.isAdmin.isChecked() ? "是" : "否";
+            if ("".equals(user)|| "".equals(pwd)){
+                Toast.makeText(LoginActivity.this,"请填写账号和密码",Toast.LENGTH_SHORT).show();
+            }else {
+                Intent intent = new Intent(LoginActivity.this,Return_valueActivity.class);
+                intent.putExtra("user",user);
+                intent.putExtra("pwd",pwd);
+                intent.putExtra("isAdmin",isAdmin);
+                startActivity(intent);
+            }
+
+            initIntent();
+        });
+    }
+
+
+
+    public static Intent newIntent(Activity activity, String te){
+        Intent intent=new Intent(activity,LoginActivity.class);
+
+        intent.putExtra(INTENT_TEXT1,te);
+
+        return intent;
+
+    }
+    private void initIntent(){
+        text1=getIntent().getStringExtra(INTENT_TEXT1);
+        Toast.makeText(this,text1,Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/intentdemo/Return_valueActivity.java
+++ app/src/main/java/com/example/intentdemo/Return_valueActivity.java
@@ -2,13 +2,35 @@ package com.example.intentdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.intentdemo.databinding.ActivityReturnValueBinding;
+
 public class Return_valueActivity extends AppCompatActivity {
+
+    private ActivityReturnValueBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_return_value);
+        mBinding= ActivityReturnValueBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        Intent intent = getIntent();
+        //获取传过来的数据
+        String user = "账号："+intent.getStringExtra("user");
+        String pwd = "密码："+intent.getStringExtra("pwd");
+        String isAdmin = "是否为管理员："+intent.getStringExtra("isAdmin");
+        //将数据显示到界面上
+        mBinding.userText2.setText(user);
+        mBinding.pwdText2.setText(pwd);
+        mBinding.isAdminText2.setText(isAdmin);
+
+        mBinding.buttonOk.setOnClickListener(v -> {
+            String R = mBinding.ReturnValue.getText().toString();
+            Intent intent1=LoginActivity.newIntent(Return_valueActivity.this,R);
+            startActivity(intent1);
+        });
     }
 }
